08197506e080fe96cd7810ab1e868e48e2cbaf7a
lindseyemaddox <lindseyemaddox@gmail.com>
2014-09-03 01:40:35 -0500

    bio - wallace?

---
+++ bios/wallace.php
@@ -0,0 +1,71 @@
+<!doctype html>
+
+<!--[if IE 9 ]><html lang="en" class="ie9"><![endif]-->
+<!--[if lt IE 9 ]><html lang="en" class="ie8"><![endif]-->
+<!--[if lt IE 8 ]><html lang="en" class="ie7"><![endif]-->
+<!--[if (gt IE 7)|!(IE)]><!--><html lang="en" class="no-js"><!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	
+	<meta name="author" content="Lindsey Maddox">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="chrome=1"> <!-- force chrome frame for IE -->
+	
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="/_styles/master.css" media="screen">
+	
+	<!-- JavaScript -->
+	<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="/_scripts/main-ck.js"></script>
+	<script src="/_scripts/jquery.fancybox.js"></script>
+
+	<title>Trust Administrator | Tennessee Risk Management Trust</title>
+	<meta name="description" content="">
+
+</head>
+
+<body>
+
+<div class="fb-bio">
+
+	<img src="/_images/team/wallace.jpg">
+
+	<div class="bio-text">
+
+		<h1>Dr. Phillip Wallace</h1>
+
+		<h2>Trust Administrator</h2>
+
+		<h3>A perennial figure in the state’s education system, Dr. Phillip Wallace joined TNRMT as Trust Administrator in 2014 after retiring as the Director of Stewart County Schools.</h3>
+
+		<h3>Dr. Wallace began his storied career as a teacher and football and basketball coach in Stewart County in 1969. He then spent 14 years as the Principal of Dover Elementary School and then another 22 years as the Director of Schools for the Stewart County Board of Education. He also has served as an adjunct professor at Christian Brothers University, Austin Peay State University, and Cumberland University.</h3>
+
+		<h3>In 2005, Wallace experienced firsthand how the Trust helps state educators when a student in his school district shot and killed a school bus driver. During the aftermath that followed, Wallace says the Trust was there every step of the way, offering assistance to everyone who was affected by the tragedy.</h3>
+
+		<h3>Now, as an employee of the Trust, he’s passionate about providing that same courteous, timely, and accurate service to clients and members. In addition to serving as CEO and executing the policies of the Trust, he’s charged with informing and advising the Board regarding Trust activities, employing and directing staff members, and maintaining adequate financial records.</h3>
+
+		<h3>And Dr. Wallace stays just as busy when he’s not in the office. In fact, some of his favorite pastimes include spending time with his family, cheering on the Tennessee Vols, working in his yard, fishing, walking his dog “Lucky,” and trying out new restaurants with his wife, just to name a few. He also supports Shriners Hospitals for Children and serves as the volunteer chairman of the Selective Service Board in Region 5.</h3>
+
+		<h3>He’s also earned several awards along the way, including being named as a:</h3>
+			
+		<ul>
+			<li>Designated Master Principal by the State of Tennessee</li>
+			<li>Distinguished State Educator by the Department of Education</li>
+			<li>Superintendent of the Year for the Mid-Cumberland District</li>
+			<li>National Alumni President of Austin Peay State University</li>
+			<li>Past Statewide Chairman of the Tennessee Superintendents Study Council</li>
+		</ul>
+
+		<h2>Did You Know?</h2>
+		
+		<h3>In addition to being an accomplished educator, instructor, innovator, team builder, facilitator, and visionary, Dr. Wallace is a seasoned musician. Not only can he carry a tune, but he also plays the drums and rhythm guitar.</h3>
+
+		<a class="textlink" href="mailto:phillipwallace@stewart.k12.tn.us">Email Phillip</a>
+
+	</div><!--bio-text-->
+
+	<div class="clear"></div>
+
+</div><!--fb-bio-->
